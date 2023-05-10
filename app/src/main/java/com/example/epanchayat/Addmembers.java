d46fbd2b5e38c17af29fbd6b4e5e0b0527b63215
Sooraj <soorajsm56@gmail.com>
2023-05-10 12:52:02 +0530

    designing members adding page

---
+++ app/src/main/java/com/example/epanchayat/Addmembers.java
@@ -0,0 +1,15 @@
+package com.example.epanchayat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addmembers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addmembers);
+
+    }
+}
