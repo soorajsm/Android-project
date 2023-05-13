e675aa3d3c9ea80992634036864a4520917acd48
Sooraj <soorajsm56@gmail.com>
2023-05-13 20:26:04 +0530

    image added to the database

--- app/src/main/java/com/example/epanchayat/Addmembers.java
+++ app/src/main/java/com/example/epanchayat/Addmembers.java
@@ -1,15 +1,121 @@
 package com.example.epanchayat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.epanchayat.databinding.ActivityAddmembersBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 public class Addmembers extends AppCompatActivity {
+
+
+
+    ActivityAddmembersBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addmembers);
+        binding = ActivityAddmembersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.chooseimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                selectImage();
+
+
+            }
+        }
+        );
+
+        binding.submitdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                uploadImage();
+
+            }
+        });
+    }
+
+
+
+    private void uploadImage() {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.show();
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        binding.memberimage.setImageURI(null);
+                        Toast.makeText(Addmembers.this, "Image uploaded Successfully", Toast.LENGTH_SHORT).show();
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                        Toast.makeText(Addmembers.this, "Failed to upload the image", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
 
     }
+
+
+    private void selectImage() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null){
+            imageUri = data.getData();
+            binding.memberimage.setImageURI(imageUri);
+        }
+    }
+
 }
