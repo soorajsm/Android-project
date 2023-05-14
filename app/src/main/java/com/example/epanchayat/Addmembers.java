15bbb565f6e8f9e95b6758f4cc12bff3f7663598
Sooraj <soorajsm56@gmail.com>
2023-05-14 12:47:05 +0530

    Data and image added to the database

--- app/src/main/java/com/example/epanchayat/Addmembers.java
+++ app/src/main/java/com/example/epanchayat/Addmembers.java
@@ -12,8 +12,12 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.epanchayat.databinding.ActivityAddmembersBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -24,10 +28,11 @@ import java.util.Locale;
 public class Addmembers extends AppCompatActivity {
 
 
-
+    String name,gender,post,age,phoneno,area;
     ActivityAddmembersBinding binding;
     Uri imageUri;
     StorageReference storageReference;
+    DatabaseReference reference;
     ProgressDialog progressDialog;
 
 
@@ -54,26 +59,63 @@ public class Addmembers extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                uploadImage();
+                uploadData();
 
             }
         });
     }
 
+    private void uploadData() {
 
 
-    private void uploadImage() {
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Uploading File....");
         progressDialog.show();
 
 
+        name=binding.membername.getText().toString();
+        gender=binding.membergender.getText().toString();
+        post=binding.memberpost.getText().toString();
+        age=binding.memberage.getText().toString();
+        phoneno=binding.memberphone.getText().toString();
+        area=binding.memberarea.getText().toString();
+
+        if(!name.isEmpty() && !gender.isEmpty() && !post.isEmpty() && !age.isEmpty() && !phoneno.isEmpty() && !area.isEmpty()) {
+
+            reference=FirebaseDatabase.getInstance().getReference("Members");
+            Members members = new Members(name, gender, post, age, phoneno, area);
+
+
+            reference.push().setValue(members).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    binding.membername.setText("");
+                    binding.membergender.setText("");
+                    binding.memberpost.setText("");
+                    binding.memberage.setText("");
+                    binding.memberphone.setText("");
+                    binding.memberarea.setText("");
+
+                    Toast.makeText(Addmembers.this, "Data submitted successfully", Toast.LENGTH_SHORT).show();
+
+                       if (progressDialog.isShowing())
+                       progressDialog.dismiss();
+
+                }
+            });
+
+        }
+
+
+
+
+
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
         Date now = new Date();
         String fileName = formatter.format(now);
         storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
-
 
         storageReference.putFile(imageUri)
                 .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -104,7 +146,34 @@ public class Addmembers extends AppCompatActivity {
     private void selectImage() {
 
         Intent intent = new Intent();
-        intent.setType("image/*");
+        intent.setType("image/*");//
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+//        Date now = new Date();
+//        String fileName = formatter.format(now);
+//        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+//
+//        storageReference.putFile(imageUri)
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//
+//                        binding.memberimage.setImageURI(null);
+//                        Toast.makeText(Addmembers.this, "Image uploaded Successfully", Toast.LENGTH_SHORT).show();
+//                        if (progressDialog.isShowing())
+//                            progressDialog.dismiss();
+//
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//
+//
+//                        if (progressDialog.isShowing())
+//                            progressDialog.dismiss();
+//                        Toast.makeText(Addmembers.this, "Failed to upload the image", Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                });
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent,100);
     }
